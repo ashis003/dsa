b292f3673e0d36ef7c68c691c5796f17917ad1a8
Ashis Kumar Singh <ashisaks03@gmail.com>
2023-03-21 04:35:25 +0530

    this commit

---
+++ arrayRotation.py
@@ -0,0 +1,13 @@
+def aRotate(arr,d):
+    for i in range(0,d):
+        temp=arr[0]
+        for j in range(0,len(arr)-1):
+            arr[j]=arr[j+1]
+        arr[-1]=temp
+def printArray(arr):
+    for i in range(0,len(arr)):
+        print(arr[i],end=" ")
+arr=[1,2,3,4,5]
+d=2
+aRotate(arr,d)
+printArray(arr)

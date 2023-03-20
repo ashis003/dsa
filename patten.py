ce955138426ba063d5f9e04274a3ab70ef804976
Ashis Kumar Singh <ashisaks03@gmail.com>
2023-03-21 04:28:51 +0530

    this commit

--- patten.py
+++ patten.py
@@ -1,10 +1,5 @@
-import array as arr
-arr = arr.array("i",[11,2,3,4,5])
-for i in range(0,4):
-    arr.reverse()
-    for j in range(4,len(arr)):
-        arr.reverse()
-        for k in range(0,len(arr)):
-            arr.reverse()
-for x in range(0,len(arr)):
-    print(arr[x])
+
+
+
+
+
